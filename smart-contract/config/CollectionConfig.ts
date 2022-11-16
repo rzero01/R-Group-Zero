2673decc911d0b6f3c5aa9d84b288c356a10bfe8
rzero01 <zeror.group@gmail.com>
2022-11-16 14:10:56 +0000

    My change

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,24 +9,24 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  contractName: 'RGroup',
+  tokenName: 'G Club Card',
+  tokenSymbol: 'GCC',
+  hiddenMetadataUri: 'ipfs://QmWo4av3eVP69FSdDKXPiCqzcpwzp4oMw6QpJM1zSCbQdw/hidden.json',
+  maxSupply: 500,
   whitelistSale: {
-    price: 0.05,
+    price: 0.00001,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 0.07,
-    maxMintAmountPerTx: 2,
+    price: 0.00002,
+    maxMintAmountPerTx: 1,
   },
   publicSale: {
-    price: 0.09,
-    maxMintAmountPerTx: 5,
+    price: 0.00003,
+    maxMintAmountPerTx: 1,
   },
-  contractAddress: null,
+  contractAddress: "0x1b94061f02d2E1A9020e686Bc590E3287Bbe81c9",
   marketplaceIdentifier: 'my-nft-token',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
